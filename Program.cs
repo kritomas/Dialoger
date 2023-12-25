85186a5cdc940254f95aeea04043eeb7fd3ddd30
kritomas <Unknown>
2023-12-25 13:23:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Main
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			dialoger.Dialog dialog = new dialoger.Dialog();
+			dialog.statements.Add(new dialoger.Statement());
+			dialog.statements.Add(new dialoger.Statement());
+			dialog.statements.Add(new dialoger.Statement());
+			dialog.statements.Add(new dialoger.Statement());
+
+			dialog[0].message = "Hello, have you heard of GNU/Linux?";
+			dialog[0].responses.Add(new dialoger.Response("Yes", 2));
+			dialog[0].responses.Add(new dialoger.Response("No", 1));
+
+			dialog[1].message = "Wdym, it's the greatest OS family in the world! It provides more functionality than Window$ and MacOS, without wasting CPU and RAM in the process! It runs on anything! Linux on a 2003 ThinkPad is more capable and useful than Window$ on an RTX 4090!";
+			dialog[1].responses.Add(new dialoger.Response("Ok, ima check it out.", 2));
+			dialog[1].responses.Add(new dialoger.Response("No, Windows best", 3));
+
+			dialog[2].message = "Hell yea, team Linux!";
+			dialog[2].responses.Add(new dialoger.Response("Hell Yeah!", -1));
+
+			dialog[3].message = "*Prepares files named CON*";
+			dialog[3].responses.Add(new dialoger.Response("Your computer ran into a problem (Error code 0x959548964515189465165, Something happened). Your windows installation is kaputt and must be reinstalled. Say goodbye to any data that wasn't backed up.\nPro tip: On Linux, the error messages tell you exactly what went wrong, allowing you to fix your installation without reinstalling and save your data.", -1));
+
+			using (FileStream writer = new FileStream("dialog.json", FileMode.Create))
+			{
+				writer.Write(Encoding.UTF8.GetBytes(dialog.Json()));
+			}
+
+			while (dialog.Talking())
+			{
+				Console.WriteLine(dialog.Now());
+				for (int i = 0; i < dialog.Now().responses.Count; i++)
+				{
+					Console.WriteLine(i + ": " + dialog.Now()[i]);
+				}
+				int response = int.Parse(Console.ReadLine());
+				dialog.Answer(response);
+			}
+		}
+	}
+}
---
+++ dialoger.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.Json;
+
+/// <summary>
+/// Namespace for dialog utilities.
+/// </summary>
+namespace dialoger
+{
+	/// <summary>
+	/// Response in a dialog.
+	/// </summary>
+	class Response
+	{
+		public string text { get; set; } = "";
+		public int goTo { get; set; } = -1;
+
+		public Response(string text, int goTo = -1)
+		{
+			this.text = text;
+			this.goTo = goTo;
+		}
+
+		public override string ToString()
+		{
+			return text;
+		}
+	}
+
+	/// <summary>
+	/// Statement in a dialog.
+	/// </summary>
+	class Statement
+	{
+		public string message { get; set; } = "";
+		public List<Response> responses { get; set; } = new List<Response>();
+
+		public Response this[int i]
+		{
+			get { return responses[i]; }
+			set { responses[i] = value; }
+		}
+
+		public override string ToString()
+		{
+			return message;
+		}
+	}
+
+	/// <summary>
+	/// Dialog itself.
+	/// </summary>
+	class Dialog
+	{
+		public List<Statement> statements { get; set; } = new List<Statement>();
+		public int current = 0;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>The current statement.</returns>
+		public Statement Now()
+		{
+			return statements[current];
+		}
+		/// <summary>
+		/// Answers to the current statement.
+		/// </summary>
+		/// <param name="responseIndex">The index of the response to the statement.</param>
+		public void Answer(int responseIndex)
+		{
+			current = Now().responses[responseIndex].goTo;
+		}
+		/// <summary>
+		/// Resets the dialog to the beginning.
+		/// </summary>
+		public void Reset()
+		{
+			current = 0;
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>The dialog is still running (true), or the dialog had already ended (false).</returns>
+		public bool Talking()
+		{
+			if (current < 0 || current >= statements.Count) return false;
+			return true;
+		}
+
+		public Statement this[int i]
+		{
+			get { return statements[i]; }
+			set { statements[i] = value; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>The dialog as JSON.</returns>
+		public string Json()
+		{
+			return JsonSerializer.Serialize(this);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="json">The JSON to parse.</param>
+		/// <returns>Dialog from the JSON.</returns>
+		public static Dialog? ParseJson(string json)
+		{
+			return JsonSerializer.Deserialize<Dialog>(json);
+		}
+	}
+}
